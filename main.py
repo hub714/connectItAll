bdbc0b659d3068714aff74643e3379866a0abedc
EC2 Default User <hubertcheung@gmail.com>
2016-12-09 17:41:22 +0000

    Poll

--- main.py
+++ main.py
@@ -2,10 +2,19 @@ from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
 import ecoAuth
 import ssl
 import boto3
+import time
 #from boto3 import dynamodb
 
 #dynamodb = boto3.resource('dynamodb')
 #table = dynamodb.Table('connectItAllSecrets')
+
+def customCallback(client, userdata, message):
+    print("Received a new message: ")
+    print(message.payload)
+    print("from topic: ")
+    print(message.topic)
+    print("--------------\n\n")
+
 apiKey = ecoAuth.get_api_key()
 print ecoAuth.get_current_token(apiKey)
 
@@ -18,7 +27,14 @@ myMQTTClient.configureDrainingFrequency(2)  # Draining: 2 Hz
 myMQTTClient.configureConnectDisconnectTimeout(10)  # 10 sec
 myMQTTClient.configureMQTTOperationTimeout(5)  # 5 sec
 
+
+
 myMQTTClient.connect()
-myMQTTClient.publish("myTopic", "myPayload", 0)
+myMQTTClient.subscribe("iot/ecobee/Python", 1, customCallback)
+loops = 0
+while True:
+    myMQTTClient.publish("iot/ecobee/Python", "myPayload"+str(loops), 0)
+    loops +=1
+    time.sleep(1)
 myMQTTClient.disconnect()
 
