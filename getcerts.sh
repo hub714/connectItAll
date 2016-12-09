d2329b2dd8f370e5f611ea1f1d90be3e7354f6c4
hub714 <hubertc@acbc3291c7d9.ant.amazon.com>
2016-12-09 00:26:56 -0800

    Plus root-CA Cert

--- getcerts.sh
+++ getcerts.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 mkdir certs
-aws s3 cp s3://huberttest/iotproject ./certs/ --recursive
+curl https://www.symantec.com/content/en/us/enterprise/verisign/roots/VeriSign-Class%203-Public-Primary-Certification-Authority-G5.pem > certs/root-CA.crt
+aws s3 cp s3://huberttest/iotproject certs/ --recursive
